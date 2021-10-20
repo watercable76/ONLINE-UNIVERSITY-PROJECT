af5c4996b5f7330829144442d3159eaf44af955c
rus19023 <rus19023@byui.edu>
2021-10-20 14:27:28 -0400

    testing node.js button to deploy to heroku

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,4 +2,11 @@ const deleteProduct = () => {
     console.log('Clicked');
     const prodId = btn.parentNode.querySelector('[name=productId]').value;
     const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+    fetch('/admin/product/' + prodId, {
+        method: 'DELETE',
+        headers: {
+            'csrf-token': csrf
+        }
+    });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -41,6 +41,6 @@ router.post(
   adminController.postEditProduct
 );
 
-router.delete("/delete-product", isAuth, adminController.postDeleteProduct);
+router.delete("/product/:productId", isAuth, adminController.deleteProduct);
 
 module.exports = router;
